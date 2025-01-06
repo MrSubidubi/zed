18d001e0e04193740abefd00f8893d135a9e4368
MrSubidubi <dev@bahn.sh>
2025-01-06 18:46:12 +0100

    Add C# autoindents for expresions, member access and based upon a semicolon present

--- extensions/csharp/languages/csharp/indents.scm
+++ extensions/csharp/languages/csharp/indents.scm
@@ -1,3 +1,11 @@
+[
+    (binary_expression)
+    (expression_statement)
+    (invocation_expression)
+    (member_access_expression)
+] @indent
+
+(_ ";" @end) @indent
 (_ "{" "}" @end) @indent
 (_ "[" "]" @end) @indent
 (_ "(" ")" @end) @indent
